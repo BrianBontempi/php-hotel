cff14aeb81f74b635374553806756daf651dbd62
BrianBontempi <brian.bont54@gmail.com>
2024-02-20 20:50:06 +0100

    added filter to see only hotels with parking

--- index.php
+++ index.php
@@ -34,17 +34,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel list</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
 </head>
 <body>
-<h1>Hotel list:</h1>
-    <ul>
-    <?php foreach ($hotels as $hotel){?>
-        <li>Name: <?php echo $hotel['name']?></li>
-        <li>Description: <?php echo $hotel['description']?></li>
-        <li>Parking: <?php echo $hotel['parking']?></li>
-        <li>Vote: <?php echo $hotel['vote']?></li>
-        <li>Distance to center: <?php echo $hotel['distance_to_center']?></li>
-    <?php } ?>
-    </ul>
+    <header class="d-flex justify-content-between">
+        <h1>Hotel list:</h1>
+
+        <form method="GET" action="">
+            <label for="filtroParcheggio">Mostra solo hotel con parcheggio:</label>
+            <input type="checkbox" name="filtroParcheggio" id="filtroParcheggio" value="1" <?php echo (isset($_GET['filtroParcheggio']) && $_GET['filtroParcheggio'] == "1") ? "checked" : ""; ?>>
+            <button type="submit">Filtra</button>
+        </form>
+    </header>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Parking</th>
+            <th scope="col">Vote</th>
+            <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel){?>
+                <tr>
+                <td><?php echo $hotel['name']?></td>
+                <td><?php echo $hotel['description']?></td>
+                <td><?php echo $hotel['parking'] ? 'Yes' : 'No'?></td>
+                <td><?php echo $hotel['vote']?></td>
+                <td><?php echo $hotel['distance_to_center']?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </body>
 </html> 
